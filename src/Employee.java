cdaee7101b4cd81d690738be3c4fbf5bb797d361
Galinaag4 <Galinaag@yandex.ru>
2022-09-04 10:27:34 +0300

    Исправления

--- src/Employee.java
+++ src/Employee.java
@@ -1,85 +1,95 @@
 import java.util.Arrays;
 
 public class Employee {
-private String fio;
-private int id;
-private double zarplata;
-private int otdel;
-private static int counter;
+    private String Fullname;
+    private int id;
+    private double Salary;
+    private int Department;
+    private static int counter;
 
 
-    public int getId() { return this.id; }
-    public String getFIO() { return this.fio; }
-    public int getOtdel() { return this.otdel; }
-    public  double getZarplata() { return this.zarplata; }
-
-
-
-    public int setOtdel() {
-        return this.otdel;
-    }
-    public  double setZarplata() {
-        return this.zarplata;
+    public int getId() {
+        return this.id;
     }
 
-
-    public Employee(String fio, int otdel, int zarplata) {
-        this.fio = fio;
-        this.otdel= otdel;
-        this.zarplata = zarplata;
-       id = ++counter;
-        System.out.println(this.fio);
-    }
-    public static int sum(Employee[]empl) {
-            double sum = 0;
-            for (int i = 0; i < empl.length; i++) {
-                sum = sum + empl[i].zarplata;
-            }
-
-        return (int) sum;
+    public String getFullname() {
+        return this.Fullname;
     }
 
-    public static double max (Employee[]empl) {
-        double max = -1;
-        for (int i =0;i< empl.length;i++){
-            if (empl[i].zarplata>max) {max=empl[i].zarplata;}
-        }
-        return max;}
+    public int getDepartment() {
+        return this.Department;
+    }
 
-    public static double min (Employee[]empl) {
-        double min =empl[0].zarplata+1;
-        for (int i =0;i< empl.length;i++){
-            if (empl[i].zarplata<min) {min=empl[i].zarplata;}
-        }
+    public double getSalary() {
+        return this.Salary;
+    }
+
+
+    public int setDepartment() {
+        return this.Department;
+    }
+
+    public double setZarplata() {
+        return this.Salary;
+    }
+
+
+    public Employee(String Fullname, int department, int salary) {
+        this.Fullname = Fullname;
+        this.Department = department;
+        this.Salary = salary;
+        id = ++counter;
+        System.out.println(this.Fullname);
+    }
+
+    public static int sum(Employee[] empl) {
+       double sum = 0;
+       for (int i = 0; i < empl.length; i++) {
+       sum = sum + empl[i].Salary;}
+       return (int) sum;}
+
+    public static double max(Employee[] empl) {
+       double max = -1;
+       for (int i = 0; i < empl.length; i++) {
+       if (empl[i].Salary > max) {
+       max = empl[i].Salary;}}
+       return max;}
+
+    public static double min(Employee[] empl) {
+        double min = empl[0].Salary + 1;
+        for (int i = 0; i < empl.length; i++) {
+        if (empl[i].Salary < min) {
+        min = empl[i].Salary;}}
         return min;}
-    public static double average (Employee[]empl) {
-        double average =sum(empl)/empl.length;
-        return (int) average;}
 
+    public static double average(Employee[] empl) {
+        double average = sum(empl) / empl.length;
+        return (int) average;
+    }
 
 
     @Override
     public String toString() {
-        return "Id: " + id + " Fio: " + fio + " Otdel: " + otdel + " Zarplata: " + zarplata ;
+        return "Id: " + id + " Fio: " + Fullname + " Otdel: " + Department + " Zarplata: " + Salary;
     }
 
     public static void main(String[] args) {
         Employee[] empl = new Employee[5];
-        empl[0] = new Employee("Иванов Иван Иванович", 1,10500);
-        empl[1] = new Employee("Петров Петр Петрович", 2,18200);
-        empl[2] = new Employee("Дмитриев Дмитрий Дмитриевич", 3,15000);
-        empl[3] = new Employee("Петров Петр Петрович", 4,30000);
-        empl[4] = new Employee("Александров Александр Александрович", 5,45000);
+        empl[0] = new Employee("Иванов Иван Иванович", 1, 10500);
+        empl[1] = new Employee("Петров Петр Петрович", 2, 18200);
+        empl[2] = new Employee("Дмитриев Дмитрий Дмитриевич", 3, 15000);
+        empl[3] = new Employee("Петров Петр Петрович", 4, 30000);
+        empl[4] = new Employee("Александров Александр Александрович", 5, 50000);
 
 
-        for (Employee e: empl) {
+        for (Employee e : empl) {
             System.out.println(e);
 
         }
-        System.out.println("Общая сумма зарплат "+sum(empl));
-        System.out.println("Максимальная зарплата "+max(empl));
-        System.out.println("Минимальная зарплата "+min(empl));
-        System.out.println("Среднее значение "+average(empl));
+        System.out.println("Общая сумма зарплат " + sum(empl));
+        System.out.println("Максимальная зарплата " + max(empl));
+        System.out.println("Минимальная зарплата " + min(empl));
+        System.out.println("Среднее значение " + average(empl));
 
 
     }
